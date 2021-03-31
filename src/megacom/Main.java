ae49e5732ac9996623df96d19b6b9f6a7312ad6c
Azamat Duisheev <azamat27071998@gmail.com>
2021-03-31 19:25:38 +0600

    main start

--- src/megacom/Main.java
+++ src/megacom/Main.java
@@ -3,29 +3,52 @@ package megacom;
 import megacom.service.StudSubjectService;
 import megacom.service.impl.StudSubjectServiceImpl;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         StudSubjectService studSubjectService = new StudSubjectServiceImpl();
         studSubjectService.ConnectionSQLite(); // connection to DB
 
-        System.out.println("-----------------Добавление нового предмета-----------------");
-        //studSubjectService.addSubject(); // add subject to DB
-        studSubjectService.selectSubject(); // get subject table from DB*/
-        System.out.println("-----------------Создание новой группы-----------------");
-        //studSubjectService.addGroup(); // add group to DB
-        studSubjectService.selectGroup(); // get groups table from DB
-        System.out.println("-----------------Создание подрупп для группы-----------------");
-        //studSubjectService.addSubgroups();
-        studSubjectService.selectSubgroups();
-        System.out.println("-----------------Добавление студента к группе-----------------");
-        //studSubjectService.addStudent();
-        studSubjectService.selectStudents();
-        System.out.println("-----------------Заполнение таблицы group_student_subject-----------------");
-        studSubjectService.fill_group_student_subject();
-        System.out.println("-----------------Проставить академ.успевамость-----------------");
-        studSubjectService.addAttendance();
+        System.out.println("1. Добавить предмет \n" +
+                "2. Создать новую группу \n" +
+                "3. Создать подгруппу для группы \n" +
+                "4. Добавть студента к группе \n" +
+                "5. Регистрация студента на предмет \n" +
+                "6. Проставить академ.успеваемость \n" +
+                "7. Выход из меню (Любая кнопка)");
+        System.out.print("Выберите кнопку: ");
+        int n = scanner.nextInt();
+
+        do{
+            if (n == 1) {
+                System.out.println("-----------------Добавление нового предмета-----------------");
+                studSubjectService.addSubject(); // add subject to DB
+                studSubjectService.selectSubject(); // get subject table from DB
+            } else if(n == 2){
+                System.out.println("-----------------Создание новой группы-----------------");
+                studSubjectService.addGroup(); // add group to DB
+                studSubjectService.selectGroup(); // get groups table from DB
+            } else if(n == 3){
+                System.out.println("-----------------Создание подрупп для группы-----------------");
+                studSubjectService.addSubgroups();
+                studSubjectService.selectSubgroups();
+            } else if(n == 4){
+                System.out.println("-----------------Добавление студента к группе-----------------");
+                studSubjectService.addStudent();
+                studSubjectService.selectStudents();
+            } else if(n == 5){
+                System.out.println("-----------------Регистрация студента на предмет-----------------");
+                studSubjectService.fill_group_student_subject();
+            } else if(n == 6){
+                System.out.println("-----------------Проставить академ.успевамость-----------------");
+                studSubjectService.addAttendance();
+            }
+        }while(n>=1 && n<=6);
 
 
         studSubjectService.close(); // close connetion DB
--- src/megacom/service/impl/StudSubjectServiceImpl.java
+++ src/megacom/service/impl/StudSubjectServiceImpl.java
@@ -83,11 +83,24 @@ public class StudSubjectServiceImpl implements StudSubjectService {
     @Override
     public void addStudent() {
         try{
+            statement = connection.createStatement();
+
             System.out.print("Введите имя студента: ");
             String name = scanner.next();
             System.out.print("Введите фамилию студента: ");
             String fam = scanner.next();
-            System.out.print("Введите группу: ");
+
+            // Вывод названий групп для удобства выбора пользователя
+            String query1 = "SELECT id, group_name FROM groups";
+            ResultSet rs1 = statement.executeQuery(query1);
+            while(rs1.next()) {
+                int id1 = rs1.getInt("id");
+                String name1 = rs1.getString("group_name");
+                System.out.println(id1+ "\t|"+ name1);
+            }
+            rs1.close();
+
+            System.out.print("Выберите группу: ");
             String group = scanner.next();
 
             int id = id_subgroups(group);
@@ -98,8 +111,8 @@ public class StudSubjectServiceImpl implements StudSubjectService {
             String query = "INSERT INTO students(firstname, secondname, id_subgroups)"+
                     " VALUES('"+students.getFirstname()+"','"+students.getSecondname()+"','"+students.getId_subgroup()+"')";
 
-            Statement statement = connection.createStatement();
             statement.executeUpdate(query);
+            statement.close();
 
         }catch (Exception e){
             System.out.println(e.getMessage());
@@ -110,7 +123,8 @@ public class StudSubjectServiceImpl implements StudSubjectService {
     private int id_group(int id) {
         try{
             statement = connection.createStatement();
-            String query = "SELECT subgroup_name FROM subgroups  ";
+            String query = "select subgroups.subgroup_name from subgroups " +
+                    "inner join groups on groups.id = subgroups.id_group where groups.group_name = '"++"' ";
 
             ResultSet rs = statement.executeQuery(query);
 
@@ -118,7 +132,7 @@ public class StudSubjectServiceImpl implements StudSubjectService {
             int i=0;
             while(rs.next()){
                 subgroup_names.add(rs.getString("subgroup_name"));
-                //System.out.println(subgroup_names.get(i));
+                System.out.println(subgroup_names.get(i));
                 i++;
             }
 
@@ -318,6 +332,9 @@ public class StudSubjectServiceImpl implements StudSubjectService {
     }
 
     private void addMarkAndCheckedStud(int id_gss) {
+        System.out.print("Введите тип занаятия - ");
+        String lessonType = scanner.next();
+
         int check;
         //do {
             System.out.print("Посещаемость 1-был(а) | 0-не был(а) ==> ");
@@ -360,8 +377,8 @@ public class StudSubjectServiceImpl implements StudSubjectService {
 
         try{
 
-            String query = "INSERT INTO attendance(check_stud, mark, lesson_date, id_group_student_subject) "+
-                    "VALUES ('"+check+"', '"+mark+"', '"+dd+"', '"+id_gss+"')";
+            String query = "INSERT INTO attendance(check_stud, mark, lesson_date, id_group_student_subject, lesson_type) "+
+                    "VALUES ('"+check+"', '"+mark+"', '"+dd+"', '"+id_gss+"', '"+lessonType+"')";
 
             statement = connection.createStatement();
             statement.executeUpdate(query);
